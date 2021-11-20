5c0eb99a17bd2b9ee97b29ed680a440acaa7041e
Thiago Henriques <thiagokxl5@gmail.com>
2021-11-20 12:09:53 -0300

    Criação da controller e da view Fonecedor

--- AppMvcTreinamentoBasico/Models/Fornecedor.cs
+++ AppMvcTreinamentoBasico/Models/Fornecedor.cs
@@ -14,7 +14,7 @@ namespace AppMvcTreinamentoBasico.Models
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é Obrigatório")]
-        [StringLength(11, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 14)]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
         public string Documento { get; set; }
 
         public TipoFornecedor TipoFornecedor { get; set; }
--- AppMvcTreinamentoBasico/Startup.cs
+++ AppMvcTreinamentoBasico/Startup.cs
@@ -1,4 +1,4 @@
-using AppMvcTreinamentoBasico.Data;
+﻿using AppMvcTreinamentoBasico.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -35,6 +35,8 @@ namespace AppMvcTreinamentoBasico
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
