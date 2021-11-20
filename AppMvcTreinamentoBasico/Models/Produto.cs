fda9218afb370095597589e098d52eeff432f05f
Thiago Henriques <thiagokxl5@gmail.com>
2021-11-20 11:27:51 -0300

    Adicionar arquivos de projeto.

---
+++ AppMvcTreinamentoBasico/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcTreinamentoBasico.Models
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorId { get; set; }
+
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public Decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
